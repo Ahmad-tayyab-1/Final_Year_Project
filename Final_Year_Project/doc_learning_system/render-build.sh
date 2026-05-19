24151062b0d345f75d86cecdf67dfcbc38fcd78f
Ahmad-tayyab-1 <147137391+Ahmad-tayyab-1@users.noreply.github.com>
2026-05-19 22:42:56 +0500

    render script updated

--- Final_Year_Project/doc_learning_system/render-build.sh
+++ Final_Year_Project/doc_learning_system/render-build.sh
@@ -2,7 +2,7 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements.txt
+pip install -r ../requirements.txt
 
 python doc_learning_system/manage.py collectstatic --no-input
 python doc_learning_system/manage.py migrate
